7dccfba0da2816bff6fd43f40b17d977e35478dc
iminsane44 <iminsane44@gmail.com>
2022-02-06 19:49:45 +0800

    first commit

---
+++ resources/js/components/WeatherAppComponent.vue
@@ -0,0 +1,158 @@
+<template>
+    
+    <div id="mainapp"  :class ="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'sunny':''">
+        
+        <main>
+        
+            <div class="form-group">
+                
+                <div>
+                    <input  placeholder="Country/City" type="text" class="w-50 offset-3 form-control" v-model="query" @keypress="fetchWeather">
+                    <!-- {{ query }} -->
+                </div>
+               
+            </div>
+
+                <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+                    <div class="location-box">
+                        <div class="location">
+                            {{ weather.name }}, {{ weather.sys.country }}
+                        </div>
+                            
+                         
+                        <div class="date"> {{ dateBuilder() }} </div>
+                    </div>
+                    <div class="weather-box">
+                         <div class="temp">
+                             {{ Math.round(weather.main.temp) }}°C
+                         </div>
+                         <div class="weather">
+                             {{ weather.weather[0].main }}
+                         </div>
+                    </div>
+                 </div>   
+
+        </main>
+
+    </div>    
+
+</template>
+
+<script>
+export default{
+name: "WeatherAppComponent.vue",
+    data(){
+        return{
+            query: '',
+            app_key: '7862562e8c4afdf7accd95ffeda4515d',
+            base_url: 'https://api.openweathermap.org/data/2.5/',
+            weather: {},
+        }
+    },
+    methods:{
+        
+        fetchWeather:function(e){
+            if(e.key == 'Enter'){
+                fetch(`${this.base_url}weather?q=${this.query}&units=metric&APPID=${this.app_key}`).then((res) => {
+                    return res.json();
+                }).then(this.setResults)
+            }
+        },
+
+            setResults:function(results){
+            this.weather = results;
+           
+            },
+
+            dateBuilder:function(){
+                
+                let d = new Date();
+
+                let days = [
+                    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+                ];
+
+                let months = [
+                    "January","February","March","April","May","June","July",
+                    "August","September","October","November","December"
+                ];
+
+                let day = days[d.getDay()];
+                let month = months[d.getMonth()].substring(0,3);
+                let date = d.getDate();
+                let year = d.getFullYear();
+
+                
+
+                return `${day} ${month} ${date}, ${year}`;
+
+            }
+
+    }
+
+}
+</script>
+
+<style scoped>
+#mainapp{
+    background-image: url("/assets/coldgif.gif");
+    background-size: cover;
+    background-position: center;
+    transition: 0.4s;
+
+}
+#mainapp.sunny{
+    background-image: url("/assets/sunny.gif");
+    /* background-size: cover;
+    background-position: bottom;
+    transition: 0.4s; */
+}
+main{
+    min-height: 100vh;
+    padding: 25px;
+    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+.location-box .location{
+    color: #fff;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+
+}
+.location-box .date{
+    color: #fff;
+    font-size: 20px;
+    font-weight: 300;
+    text-align: center;
+    font-style: italic;
+
+}
+.weather-box{
+    text-align: center;
+
+}
+.weather-box .temp{
+    display: inline-block;
+    padding: 10px 25px;
+    color: #fff;
+    font-size: 102px;
+    font-weight: 900;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.25);;
+    border-radius: 16px;
+    margin: 30px 0px;
+    box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+
+}
+.weather-box .weather{
+    color: #fff;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+
+}
+
+</style>
+
---
+++ resources/views/vue.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    <div id="mainapp"> 
+
+        <vue-component>
+            
+        </vue-component>
+
+
+    </div>
+@endsection
